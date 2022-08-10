fe8e5ef4754c311424187c0704aca38517a0010e
nomad2569 <nomad2569@naver.com>
2022-08-10 21:46:20 +0900

    jwt 에 sign_id 와 lol_name 저장

--- controller/user.py
+++ controller/user.py
@@ -14,63 +14,85 @@ algorithm = os.environ.get("ALGORITHM")
 
 def get_user_info(user_id):
     rds_conn = get_rds_db_connection()
-    user_data = {
-        "user_id": user_id
-    }
-    
+    user_data = {"user_id": user_id}
+
     user_response = exec_query(
-        rds_conn, query.SELECT_USER, True, input_params=user_data)
+        rds_conn, query.SELECT_USER, True, input_params=user_data
+    )
 
     return user_response
+
 
 # def insert_user_info(email, password):
 #     rd
 
 
-async def sign_in(email, password):
+async def sign_in(email: str, password: str):
     is_exist = get_user_info(email)
     if is_exist:
         is_verified = bcrypt.checkpw(
-            password.encode('utf-8'), bcrypt.hashpw(is_exist[0]['password'].encode("utf-8"), bcrypt.gensalt()))
-        # 회원가입시 비밀번호를 암호화해서 넣는걸로 만든 후에 바꾸기 
+            password.encode("utf-8"),
+            bcrypt.hashpw(is_exist[0]["password"].encode("utf-8"), bcrypt.gensalt()),
+        )
+        # 회원가입시 비밀번호를 암호화해서 넣는걸로 만든 후에 바꾸기
         # if not user_info.email or not user_info.pw:
         #     return JSONResponse(status_code=400, content=dict(msg="Email and PW must be provided'"))
         # if not is_exist:
         #     return JSONResponse(status_code=400, content=dict(msg="NO_MATCH_USER"))
         # user = Users.get(email=user_info.email)
-        # is_verified = bcrypt.checkpw(user_info.pw.encode("utf-8"), user.pw.encode('utf-8'))    
-        
+        # is_verified = bcrypt.checkpw(user_info.pw.encode("utf-8"), user.pw.encode('utf-8'))
+
         if is_verified:
-            token = jwt.encode({"email": email}, secret_key, algorithm)
+            conn = get_rds_db_connection()
+
+            lol_names = exec_query(
+                conn, query.GET_USER_LOL_ACCOUNT, input_params={"email": email}
+            )
+
+            if not lol_names and len(lol_names) <= 0:
+                return JSONResponse(
+                    status_code=400, content=dict(msg="아이디에 해당하는 롤 계정이 존재하지 않습니다.")
+                )
+
+            token = jwt.encode(
+                {"email": email, "lol_name": lol_names[0].get("LOL_NAME", "")},
+                secret_key,
+                algorithm,
+            )
+
             return token
             # return {"access_token": token, "token_type": "bearer"}
 
-        #아이디는 있는데 비밀번호가 틀림
+        # 아이디는 있는데 비밀번호가 틀림
         else:
             return JSONResponse(status_code=400, content=dict(msg="패스워드가 일치하지 않습니다."))
+
     else:
-        #아이디(이메일)가 없음, 회원정보 없음
+        # 아이디(이메일)가 없음, 회원정보 없음
         return JSONResponse(status_code=400, content=dict(msg="등록된 회원이 아닙니다."))
 
-def register(argument:UserRegisterArgument):
+
+def register(argument: UserRegisterArgument):
     rds_conn = get_rds_db_connection()
-    user_data = {
-        "email": argument.get("email"),
-        "password" : argument.get("password")
-    }
-    
+    user_data = {"email": argument.get("email"), "password": argument.get("password")}
+
     print("############### user_info 생성 ###############")
-    return exec_insert_query(rds_conn, query.INSERT_USER_REGISER, input_params=user_data)
+    exec_insert_query(rds_conn, query.INSERT_USER_REGISER, input_params=user_data)
+
+    return JSONResponse(status_code=200, content=dict(msg="회원가입 성공"))
+
 
 # 미지의 길...이메일 본인인증......
 def email_auth(email):
-    #이미 존재하는 회원인지 확인
+    # 이미 존재하는 회원인지 확인
     is_exist = get_user_info(email)
     if is_exist:
-        return JSONResponse(status_code=400, content=dict(msg="동일한 이메일로 가입한 회원이 존재합니다."))
-    #없는 회원이면 인증 이메일 발송
+        return JSONResponse(
+            status_code=400, content=dict(msg="동일한 이메일로 가입한 회원이 존재합니다.")
+        )
+    # 없는 회원이면 인증 이메일 발송
     # else:
-        
-        # 디비에 비번 넣을떄 : hash_pw = bcrypt.hashpw(reg_info.pw.encode("utf-8"), bcrypt.gensalt())
-    #인증 처리
+
+    # 디비에 비번 넣을떄 : hash_pw = bcrypt.hashpw(reg_info.pw.encode("utf-8"), bcrypt.gensalt())
+    # 인증 처리
     return True
--- query/user.py
+++ query/user.py
@@ -1,28 +1,37 @@
 # 유저 정보 조회
 SELECT_USER = """
-    SELECT *
-      FROM USER
-     WHERE signin_id = %(user_id)s
-    ;
-    """
+SELECT *
+  FROM USERS.USER
+ WHERE signin_id = %(user_id)s
+;
+"""
 
 # 유저 정보 등록 -- 최초 회원가입 시
 INSERT_USER_REGISER = """
-    INSERT INTO LoLing.USER(
-        signin_id,
-        password,
-        manner_tier,
-        like_cnt,
-        hate_cnt,
-        created_at,
-        updated_at
-    ) VALUES(
-        %(email)s,
-        %(password)s,
-        '골드',
-        0,
-        0,
-        now(),
-        null
-    )
-    """
+INSERT INTO USERS.USER (
+       signin_id
+     , password
+     , manner_tier
+     , like_cnt
+     , hate_cnt
+     , created_at
+     , updated_at
+) VALUES (
+     , %(email)s
+     , %(password)s
+     , '골드'
+     , 0
+     , 0
+     , NOW()
+     , NULL
+)
+"""
+
+GET_USER_LOL_ACCOUNT = """
+SELECT B.lol_name as lol_name
+  FROM USERS.USER A
+  LEFT OUTER JOIN USERS.USER_LOL_ACCOUNT_MAP B
+    ON A.SIGNIN_ID = B.SIGNIN_ID
+ WHERE A.SIGNIN_ID = %(email)s
+;
+"""
