cb369c156b1c2dbac3a7302ab5b6a6d7c0b6464d
summer-bonobono <fmadudid@gmail.com>
2022-09-11 20:33:56 +0900

    total kda, win rate 가져오기

--- query/mart/mart_user_total.sql
+++ query/mart/mart_user_total.sql
@@ -2,10 +2,27 @@
 SELECT lol_name
      , COUNT(*) as total_count
      , AVG(IF(win, 1, 0)) as total_win_rate
-     , IF(AVG(kills + assists - deaths) > 0, AVG(kills + assists - deaths), 0) as total_kda
+     , IF(deaths = 0, AVG((kills + assists) / deaths), AVG((kills + assists)* 1.2)) as total_kda
   FROM MATCHES.USERS_MATCH_HISTORY A
- WHERE A.lol_name = '꼽 죽'
+ WHERE A.lol_name = '고려대 김자헌'
  GROUP BY lol_name
  ORDER BY total_count desc
 -- limit 2
+;
+
+# 전체 승률, 전체 kda update
+UPDATE USERS.LOL_ACCOUNT B
+  LEFT OUTER JOIN
+      (SELECT lol_name
+     , COUNT(*) as total_count
+     , AVG(IF(win, 1, 0)) as total_win_rate
+     , IF(deaths = 0, AVG((kills + assists) / deaths), AVG((kills + assists)* 1.2)) as total_kda
+  FROM MATCHES.USERS_MATCH_HISTORY A
+ WHERE A.lol_name = '고려대 김자헌'
+ GROUP BY lol_name
+ ORDER BY total_count desc) C
+    ON C.lol_name=B.lol_name
+   SET B.total_win_rate = C.total_win_rate
+     , B.total_kda = C.total_kda
+ WHERE C.lol_name=B.lol_name
 ;
